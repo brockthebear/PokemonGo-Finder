e57beaa538ed13adc3a832ca03a1db4bb42f6452
Brock Boren <brock.boren@gmail.com>
2016-07-21 10:39:54 -0500

    Fixes notifier text

--- notifier.py
+++ notifier.py
@@ -37,10 +37,8 @@ def pokemon_found(pokemon):
     google_maps_link = "http://maps.google.com/maps?q=" + str(pokemon["lat"]) + "," + str(pokemon["lng"]) + ',20z'
     notification_text = "Pokemon Finder found " + _str(pokemon["name"]) + "!"
     disappear_time = str(datetime.fromtimestamp(pokemon["disappear_time"]).strftime("%I:%M%p").lstrip('0'))+")"
-    location_text = "Locate on Google Maps : " + gMaps + ". " + _str(pokemon["name"]) + " will be available until " + disappear_time + "."
+    location_text = "Locate on Google Maps : " + google_maps_link + ". " + _str(pokemon["name"]) + " will be available until " + disappear_time + "."
 
     push = pushbullet_client.push_link(notification_text, google_maps_link, body=location_text)
-
-
 
 init()
